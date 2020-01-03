e650eb1733d2a1e007540f0878d987c64e867457
YOUNGmaxer <517538470@qq.com>
2020-01-04 00:18:24 +0800

    transitionGroup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Component, Fragment } from 'react';
-import { CSSTransition } from 'react-transition-group';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import './App.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      show: true
+      show: true,
+      list: []
     }
     this.handleToggle = this.handleToggle.bind(this);
+    this.handleAdd = this.handleAdd.bind(this);
   }
 
   render() {
@@ -24,7 +26,27 @@ class App extends Component {
         >
           <div>hello</div>
         </CSSTransition>
+        <TransitionGroup>
+          {
+            this.state.list.map((item, index) => {
+              return (
+                <CSSTransition
+                  in={this.state.show}
+                  timeout={1000}
+                  classNames="fade"
+                  unmountOnExit
+                  onEnter={(el) => {el.style.color = 'blue'}}
+                  appear={true}
+                  key={index}
+                >
+                  <div>{item}</div>
+                </CSSTransition>
+              )
+            })
+          }
+        </TransitionGroup>
         <button onClick={this.handleToggle}>toggle</button>
+        <button onClick={this.handleAdd}>add</button>
       </Fragment>
     )
   }
@@ -32,7 +54,13 @@ class App extends Component {
   handleToggle() {
     this.setState(() => {
       return { show: !this.state.show }
-    })
+    });
+  }
+
+  handleAdd() {
+    this.setState(() => {
+      return { list: [...this.state.list, 'item'] }
+    });
   }
 }
 
