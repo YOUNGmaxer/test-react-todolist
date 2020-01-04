09f55eebdb09630361c6fa66efef8dbf2bc26d5c
YOUNGmaxer <517538470@qq.com>
2020-01-04 14:10:04 +0800

    redux实现todolist

--- src/AntdTodoList.js
+++ src/AntdTodoList.js
@@ -1,31 +1,69 @@
 import React, { Component } from 'react';
 import { Input, Button, List } from 'antd';
 import 'antd/dist/antd.css';
-
-const data = [
-  'Racing car sprays burning fuel into crowd.',
-  'Japanese princess to wed commoner.',
-  'Australian walks 100km after outback crash.',
-  'Man charged over missing wedding girl.',
-  'Los Angeles battles huge wildfires.',
-];
+import store from './store';
 
 class AntdTodoList extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = store.getState();
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleStoreChange = this.handleStoreChange.bind(this);
+    this.handleBtnClick = this.handleBtnClick.bind(this);
+    store.subscribe(this.handleStoreChange);
+  }
+
   render() {
     return (
       <div style={{margin: '10px'}}>
         <div style={{marginBottom: '10px'}}>
-          <Input placeholder="input" style={{width: '300px', marginRight: '10px'}}/>
-          <Button type="primary">提交</Button>
+          <Input
+            value={this.state.inputValue}
+            placeholder="input"
+            style={{width: '300px', marginRight: '10px'}}
+            onChange={this.handleInputChange}
+          />
+          <Button
+            type="primary"
+            onClick={this.handleBtnClick}
+          >提交</Button>
         </div>
         <List
           bordered
-          dataSource={data}
-          renderItem={item => <List.Item>{item}</List.Item>}
+          dataSource={this.state.list}
+          renderItem={(item, index) => <List.Item onClick={this.handleItemDelete.bind(this, index)}>{item}</List.Item>}
           style={{width: '300px'}}
         />
       </div>
     )
+  }
+
+  handleInputChange(e) {
+    const action = {
+      type: 'change_input_value',
+      value: e.target.value
+    }
+    store.dispatch(action);
+  }
+
+  handleStoreChange() {
+    this.setState(store.getState());
+  }
+
+  handleBtnClick() {
+    const action = {
+      type: 'add_todo_item'
+    };
+    store.dispatch(action);
+  }
+
+  handleItemDelete(index) {
+    const action = {
+      type: 'delete_todo_item',
+      index
+    };
+    store.dispatch(action);
   }
 }
 
